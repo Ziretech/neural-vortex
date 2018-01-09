04f0cfe2d4f633fe931388adcd174a790eaa2515
Josef Höglund <josef.hoglund@gmail.com>
2018-01-09 21:21:23 +0100

    Skapar HinderkartaMock.

---
+++ NeuralVortex/UseCase.NeuralVortex.Spec/HinderkartaMock.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UseCase.NeuralVortex.Kontroll;
+using UseCase.NeuralVortex.Spelvärld;
+
+namespace UseCase.NeuralVortex.Spec
+{
+    public class HinderkartaMock : IHinderkarta
+    {
+        private readonly Spelvärldsposition[] _hinder;
+
+        public HinderkartaMock(Spelvärldsposition[] hinder)
+        {
+            _hinder = hinder;
+        }
+
+        public bool Hindrar(Spelvärldsposition position)
+        {
+            return _hinder.Any(h => h.Equals(position));
+        }
+    }
+}
