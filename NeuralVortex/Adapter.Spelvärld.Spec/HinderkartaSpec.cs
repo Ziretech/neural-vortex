d85f8152bceeb9862306aa6e2300d4113883040e
Josef Höglund <josef.hoglund@gmail.com>
2018-01-09 21:42:05 +0100

    Skapar Hinderkarta och testprojekt för Adapter.Spelvärld.

---
+++ NeuralVortex/Adapter.Spelvärld.Spec/HinderkartaSpec.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.NeuralVortex.Spelvärld;
+
+namespace Adapter.Spelvärld.Spec
+{
+    [TestFixture]
+    public class HinderkartaSpec
+    {
+        [Test]
+        public void Kan_skapas_utan_hinder()
+        {
+            var hinderkarta = new Hinderkarta(null, 0);
+            Assert.That(!hinderkarta.Hindrar(new Spelvärldsposition(1, 2)));
+        }
+    }
+}
---
+++ NeuralVortex/Adapter.Spelvärld/Hinderkarta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.NeuralVortex.Kontroll;
+using UseCase.NeuralVortex.Spelvärld;
+
+namespace Adapter.Spelvärld
+{
+    public class Hinderkarta : IHinderkarta
+    {
+        public Hinderkarta(bool[] hinder, int kartbredd)
+        {
+
+        }
+
+        public bool Hindrar(Spelvärldsposition position)
+        {
+            return false;
+        }
+    }
+}
