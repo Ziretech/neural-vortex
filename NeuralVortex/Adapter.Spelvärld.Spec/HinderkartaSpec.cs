a36516ac08a01df0710a89280865aaab614dc7d8
Josef Höglund <josef.hoglund@gmail.com>
2018-01-09 21:57:13 +0100

    Implementerade hinderkarta som representerare en karta med hinder.

--- NeuralVortex/Adapter.Spelvärld.Spec/HinderkartaSpec.cs
+++ NeuralVortex/Adapter.Spelvärld.Spec/HinderkartaSpec.cs
@@ -17,5 +17,61 @@ namespace Adapter.Spelvärld.Spec
             var hinderkarta = new Hinderkarta(null, 0);
             Assert.That(!hinderkarta.Hindrar(new Spelvärldsposition(1, 2)));
         }
+
+        [Test]
+        public void Hindrar_när_kartans_enda_fält_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { true }, 1);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(0, 0)));
+        }
+
+        [Test]
+        public void Hindrar_inte_när_kartans_enda_fält_inte_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false }, 1);
+            Assert.That(!hinderkarta.Hindrar(new Spelvärldsposition(0, 0)));
+        }
+
+        [Test]
+        public void Hindrar_när_kartans_tredje_fält_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, false, true }, 3);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(2, 0)));
+        }
+
+        [Test]
+        public void Hindrar_när_kartans_tredje_fält_är_utanför_kartan()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, false }, 2);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(2, 0)));
+        }
+
+        [Test]
+        public void Hindrar_när_kartans_tredje_fält_är_utanför_kartan_åt_vänster()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, false }, 2);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(-1, 0)));
+        }
+
+        [Test]
+        public void Hindrar_när_kartans_andra_fält_på_höjden_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, true }, 1);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(0, 1)));
+        }
+
+        [Test]
+        public void Hindrar_när_2x2_kartans_3_fält_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, false, true, false }, 2);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(0, 1)));
+        }
+
+        [Test]
+        public void Hindrar_när_2x2_kartans_4_fält_är_ett_hinder()
+        {
+            var hinderkarta = new Hinderkarta(new bool[] { false, false, false, true }, 2);
+            Assert.That(hinderkarta.Hindrar(new Spelvärldsposition(1, 1)));
+        }
     }
 }
--- NeuralVortex/Adapter.Spelvärld/Hinderkarta.cs
+++ NeuralVortex/Adapter.Spelvärld/Hinderkarta.cs
@@ -10,13 +10,25 @@ namespace Adapter.Spelvärld
 {
     public class Hinderkarta : IHinderkarta
     {
+        private bool[] _hinder;
+        private readonly int _kartbredd;
+
         public Hinderkarta(bool[] hinder, int kartbredd)
         {
-
+            _hinder = hinder;
+            _kartbredd = kartbredd;
         }
 
         public bool Hindrar(Spelvärldsposition position)
         {
+            if(_hinder != null)
+            {
+                if(position.X < _kartbredd && position.X >= 0)
+                {
+                    return _hinder[position.X + position.Y * _kartbredd];
+                }
+                return true;
+            }
             return false;
         }
     }
