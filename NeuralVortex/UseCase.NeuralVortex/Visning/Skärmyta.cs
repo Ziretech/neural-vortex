2dbc25ed8f26abb554c552e5f63d647fa964a60a
Josef Höglund <josef.hoglund@gmail.com>
2018-01-24 23:34:23 +0100

    Skärmyta är en Yta.

--- NeuralVortex/UseCase.NeuralVortex/Visning/Skärmyta.cs
+++ NeuralVortex/UseCase.NeuralVortex/Visning/Skärmyta.cs
@@ -3,22 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UseCase.NeuralVortex.Spec;
 
 namespace UseCase.NeuralVortex.Visning
 {
-    public class Skärmyta
+    public class Skärmyta : Yta
     {
-        private readonly int _bredd;
-        private readonly int _höjd;
-
-        public Skärmyta(int bredd, int höjd)
-        {
-            _bredd = bredd;
-            _höjd = höjd;
-        }
-
-        public int Bredd => _bredd;
-
-        public int Höjd => _höjd;
+        public Skärmyta(int bredd, int höjd) : base(bredd, höjd) { }
     }
 }
