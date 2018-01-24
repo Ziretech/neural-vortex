ae83c48270a14ddbbe4c3c6fa17b6fade5cfc326
Josef Höglund <josef.hoglund@gmail.com>
2018-01-24 23:26:12 +0100

    Yta definierar en yta (otypad).

---
+++ NeuralVortex/UseCase.NeuralVortex.Spec/YtaSpec.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase.NeuralVortex.Spec
+{
+    [TestFixture]
+    public class YtaSpec
+    {
+        [TestCase(1, 2)]
+        [TestCase(5, 7)]
+        public void Skapas_med_en_bredd_och_höjd(int bredd, int höjd)
+        {
+            var yta = new Yta(bredd, höjd);
+            Assert.That(yta.Bredd, Is.EqualTo(bredd));
+            Assert.That(yta.Höjd, Is.EqualTo(höjd));
+        }
+    }
+}
---
+++ NeuralVortex/UseCase.NeuralVortex/Yta.cs
@@ -0,0 +1,17 @@
+﻿namespace UseCase.NeuralVortex.Spec
+{
+    public class Yta
+    {
+        private readonly int _bredd;
+        private readonly int _höjd;
+
+        public Yta(int bredd, int höjd)
+        {
+            _bredd = bredd;
+            _höjd = höjd;
+        }
+
+        public int Bredd => _bredd;
+        public int Höjd => _höjd;
+    }
+}
