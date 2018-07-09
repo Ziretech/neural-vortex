d7b674893a4303d7856b5dc922bb77a88daaf46c
Josef Höglund <josef.hoglund@gmail.com>
2018-07-09 17:25:14 +0200

    Sekvensförfyttning

---
+++ NeuralVortex/UseCase.NeuralVortex.Spec/AI/SekvensFörflyttningSpec.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.NeuralVortex.AI;
+using UseCase.NeuralVortex.Spelvärld;
+
+namespace UseCase.NeuralVortex.Spec.AI
+{
+    [TestFixture]
+    public class SekvensFörflyttningSpec
+    {
+        [Test]
+        public void HarNästaRiktningÄrHöger()
+        {
+            var förflyttning = new SekvensFörflyttning();
+            Assert.That(förflyttning.NästaRiktning, Is.EqualTo(new Spelvärldsposition(1, 0)));
+        }
+        [Test]
+        public void HarNästaRiktningÅtVänster()
+        {
+            var förflyttning = new SekvensFörflyttning();
+            Assert.That(förflyttning.NästaRiktning, Is.EqualTo(new Spelvärldsposition(-1, 0)));
+        }
+    }
+}
---
+++ NeuralVortex/UseCase.NeuralVortex/AI/SekvensFörflyttning.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase.NeuralVortex.Spelvärld;
+
+namespace UseCase.NeuralVortex.AI
+{
+    public class SekvensFörflyttning : IRiktningsgenerator
+    {
+        public Spelvärldsposition NästaRiktning => new Spelvärldsposition(1, 0);
+    }
+}
