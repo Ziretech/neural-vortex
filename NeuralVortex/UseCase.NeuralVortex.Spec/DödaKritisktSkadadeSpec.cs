d20736b9d635613d1af1f4241e9f8694f61f8b7e
Josef Höglund <josef.hoglund@gmail.com>
2018-07-30 10:38:31 +0200

    Döda kritiskt skadade avslutar inte spelet om huvudkaraktären saknas.

--- NeuralVortex/UseCase.NeuralVortex.Spec/DödaKritisktSkadadeSpec.cs
+++ NeuralVortex/UseCase.NeuralVortex.Spec/DödaKritisktSkadadeSpec.cs
@@ -11,11 +11,30 @@ namespace UseCase.NeuralVortex.Spec
     public class DödaKritisktSkadadeSpec
     {
         [Test]
-        [Ignore("Fixa hantering av speletsfortsättning i uppdatera spelvärlden först")]
         public void Avslutar_inte_spelet_om_huvudkaraktären_saknas()
         {
-            var dödaKritisktSkadade = new DödaKritisktSkadade();
+            var spelvärld = new SpelvärldMock();
+            var dödaKritisktSkadade = new DödaKritisktSkadade(spelvärld);
             Assert.That(dödaKritisktSkadade.Döda(), Is.EqualTo(SpeletsFortsättning.Fortsätt));
         }
+
+        [Test]
+        public void Avslutar_spelet_om_huvudkaraktären_är_kritiskt_skadad()
+        {
+            var spelvärld = new SpelvärldMock();
+            spelvärld.Huvudkaraktär = new Huvudkaraktär();
+            // hur ska man sätta hälsa?
+            var dödaKritisktSkadade = new DödaKritisktSkadade(spelvärld);
+            Assert.That(dödaKritisktSkadade.Döda(), Is.EqualTo(SpeletsFortsättning.Avsluta));
+        }
+
+        // TODO: Hur ska man göra med hälsa?
+        // Interface där man kan fråga om någon är kritiskt skadad?
+        // Borde huvudkaraktären ha ett interface isåfall?
+        // Borde man isåfall slå ihop huvudkaraktären och fienden? Vad skiljer dem åt egentligen just nu?
+        // Eller ska man göra det enklaste just nu: man skapar ett fält hälsa för huvudkaraktären?
+        // Och när det är dags att göra samma för fienden så upptäcker man att/om det blir duplicering?
+
+        // TODO: Gör undantag för att skapas utan spelvärld
     }
 }
--- NeuralVortex/UseCase.NeuralVortex/DödaKritisktSkadade.cs
+++ NeuralVortex/UseCase.NeuralVortex/DödaKritisktSkadade.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UseCase.NeuralVortex.Spelvärld;
 
 namespace UseCase.NeuralVortex
 {
     internal class DödaKritisktSkadade : IDödaKritisktSkadade
     {
+        private ISpelvärld _spelvärld;
+
+        public DödaKritisktSkadade(ISpelvärld spelvärld)
+        {
+            _spelvärld = spelvärld;
+        }
+
         public SpeletsFortsättning Döda()
         {
-            throw new NotImplementedException();
+            return SpeletsFortsättning.Fortsätt;
         }
     }
 }
