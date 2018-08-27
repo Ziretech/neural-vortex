121ce76950378114965bf7528d519f8408bed701
Josef Höglund <josef.hoglund@gmail.com>
2018-08-27 20:13:51 +0200

    Refaktorerade brickfält.

--- NeuralVortex/Adapter.OpenTK/Grafik/Brickfält.cs
+++ NeuralVortex/Adapter.OpenTK/Grafik/Brickfält.cs
@@ -14,35 +14,33 @@ namespace Adapter.OpenTK.Grafik
         private readonly Kamera _kamera;
         private readonly IPositionskonverterare _konverterare;
         private readonly Bricka[] _definitioner;
-        private readonly int _kartbredd;
-        private readonly int _karthöjd;
         private readonly int[] _karta;
         private readonly Skärmyta _brickstorlek;
+        private readonly Skärmyta _kartstorlek;
 
         public Brickfält(Bricka[] definitioner, int[] karta, int kartbredd, Skärmyta brickstorlek)
         {
             _definitioner = definitioner;
             _karta = karta;
-            _kartbredd = kartbredd;
             _brickstorlek = brickstorlek;
-            _karthöjd = karta.Count() / kartbredd;
+            _kartstorlek = new Skärmyta(kartbredd, karta.Count() / kartbredd);
         }
 
         public Skärmyta Dimensioner
         {
             get
             {
-                return _brickstorlek.MultipliceratMed(new Skärmyta(_kartbredd, _karthöjd));
+                return _brickstorlek.MultipliceratMed(_kartstorlek);
             }
         }
 
         public void Visa(Skärmposition position)
         {
-            for(var y = 0; y < _karthöjd; y++)
+            for(var y = 0; y < _kartstorlek.Höjd; y++)
             {
-                for (var x = 0; x < _kartbredd; x++)
+                for (var x = 0; x < _kartstorlek.Bredd; x++)
                 {
-                    var kartindex = x + y * _kartbredd;
+                    var kartindex = x + y * _kartstorlek.Bredd;
                     var relativPosition = new Skärmposition(x * _brickstorlek.Bredd, y * _brickstorlek.Höjd);
                     _definitioner[_karta[kartindex]].Visa(position.Plus(relativPosition));
                 }
